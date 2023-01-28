6fe3336eb490a62855ee698a21d542441241c09a
Alex Vong <avong003@gmail.com>
2023-01-27 18:20:50 -0800

    practice disney node js file

---
+++ disney.js
@@ -0,0 +1,7 @@
+const characters = ['mickey', 'goofy', 'donald', 'tarzan', 'mulan'];
+
+const movies = ['toy story', 'tangled', 'treasure planet', 'ratatouille']
+
+
+
+module.exports = { characters, movies};
